de07b120144041e98cbe9723e23d0c56f1514580
Muhammad Zulkifli <muhammadzulkifli79@gmail.com>
2021-05-02 21:25:49 +0700

    add projek tanam

--- resources/views/page/user/project/add.blade.php
+++ resources/views/page/user/project/add.blade.php
@@ -5,7 +5,8 @@
     <div class="card-header">
         <h2 class="card-title"><strong>Buat Proyek Tanam</strong></h2>
     </div> <!-- /.card-body -->
-    <form class="form-horizontal">
+    <form class="form-horizontal" method="POST" action="{{route('project.store')}}">
+        @csrf
         <div class="card-body">
             <div class="mb-3" id="create-proyek">
                 <!-- Data Varietas Tanaman -->
@@ -15,13 +16,13 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Nama Tanaman</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="nama_tanaman">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Tahap Pencatatan Awal</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="tahap_pencatatan_awal">
                     </div>
                 </div>
                 <!-- Data Lahan -->
@@ -32,25 +33,25 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Alamat</label>
                     <div class="col-sm-9">
-                        <textarea class="form-control" rows="3" placeholder=""></textarea>
+                        <textarea class="form-control" rows="3" placeholder="" name="alamat"></textarea>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Provinsi</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="provinsi">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Kota/Kabupaten</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="kota_kab">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Koordinat</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="koordinat">
                     </div>
                 </div>
                 <!-- Data Blok -->
@@ -62,7 +63,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">Luas Blok</label>
                         <div class="input-group col-sm-2">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="data_blok[0][luas]">
                             <div class="input-group-append">
                                 <span class="input-group-text">m2</span>
                             </div>
@@ -71,7 +72,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">Jml Pokok Tanaman</label>
                         <div class="input-group col-sm-2">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="data_blok[0][jml_pokok_tanaman]">
                             <div class="input-group-append">
                                 <span class="input-group-text">pokok</span>
                             </div>
@@ -80,17 +81,18 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">Umur Tanaman</label>
                         <div class="input-group col-sm-2">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="data_blok[0][umur_tanaman]">
                             <div class="input-group-append">
                                 <span class="input-group-text">bulan</span>
                             </div>
                         </div>
                     </div>
                     <hr size="1px" width="100%">
-                    
-                    <div id="data-selanjutnya"></div>
+
                 </div>
-                                    
+
+                <div id="data-selanjutnya"></div>
+
                 <div class="btn btn-block btn-success" id="next">+ Tambah Data Blok</div>
             </div>
         </div>
@@ -108,12 +110,23 @@
 
 @section('script')
 <script>
+    var dataBlokCount = 1;
+
     $(function(){
         $("#next").click(function (e) {
-            var $data = $("#data-blok").clone();
-            $('#data-selanjutnya').html($data);
+            var data = $("#data-blok").clone();
+            data.attr('id', 'data-blok' + dataBlokCount);
+            var inputs = data.find('input');
+            for(var i=0; i<inputs.length; i++){
+                var newName = 'data_blok[' + dataBlokCount + ']';
+                var name = $(inputs[i]).attr('name').replace('data_blok[0]', newName);
+                $(inputs[i]).attr('name', name);
+                $(inputs[i]).val('');
+            }
+            $(data[0]).insertBefore('#data-selanjutnya');
             $("#next").show();
+            dataBlokCount++;
         });
     });
-    </script>
+</script>
 @endsection
